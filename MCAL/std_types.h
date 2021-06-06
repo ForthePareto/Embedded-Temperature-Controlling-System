ba7b5ee26447d11c25f0568a74a910a5b45d165d
Eslam Khaled <eslamkhaled64@gmail.com>
2021-06-06 03:05:14 +0200

    Create std_types.h

---
+++ MCAL/std_types.h
@@ -0,0 +1,47 @@
+/*
+ * std_types.h
+ *
+ *  Created on: Jun. 6, 2021
+ *      Author: Eslam Khaled Korany
+ */
+
+#ifndef MCAL_STD_TYPES_H_
+#define MCAL_STD_TYPES_H_
+
+
+
+
+typedef unsigned char         uint8;          /*           0 .. 255             */
+typedef signed char           sint8;          /*        -128 .. +127            */
+typedef unsigned short        uint16;         /*           0 .. 65535           */
+typedef signed short          sint16;         /*      -32768 .. +32767          */
+typedef unsigned long         uint32;         /*           0 .. 4294967295      */
+typedef signed long           sint32;         /* -2147483648 .. +2147483647     */
+typedef unsigned long long    uint64;         /*       0..18446744073709551615  */
+typedef signed long long      sint64;
+typedef float                 float32;
+typedef double                float64;
+
+
+/* Boolean Values */
+
+#ifndef DISABLE
+#define DISABLE       (0u)
+#endif
+
+#ifndef ENABLE
+#define ENABLE        (1u)
+#endif
+
+#ifndef FALSE
+#define FALSE       (0u)
+#endif
+
+#ifndef TRUE
+#define TRUE        (1u)
+#endif
+
+#define HIGH        (1u)
+#define LOW         (0u)
+
+#endif /* MCAL_STD_TYPES_H_ */
