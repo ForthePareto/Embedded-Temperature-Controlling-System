47317b32fb7f33a90e7e6f7346d39f4f727612cc
ASquaredM <43936923+ASquaredM@users.noreply.github.com>
2021-06-07 21:33:55 +0200

    DIO

---
+++ MCAL/DIO.h
@@ -0,0 +1,19 @@
+#ifndef DIO_H_INCLUDED
+#define DIO_H_INCLUDED
+
+#include <std_types.h>
+#include <registers.h>
+#include <common_macros.h>
+
+void DIO_setPINDir(uint8 portname,uint8 pinnumber,uint8 direction);
+void DIO_write(uint8 portname,uint8 pinnumber,uint8 outputvalue);
+uint8 DIO_readPin(uint8 portname,uint8 pinnumber);
+uint8 DIO_readPort(uint8 portname);
+void DIO_togglePin(uint8 portname,uint8 pinnumber);
+void DIO_writePort(uint8 portname,uint8 portvalue);
+void clear_highNibble(uint8 portname) ;
+void DIO_writeHighnibble(uint8 portname,uint8 value);
+void DIO_connectPullup(uint8 portname ,uint8 pinnumber, uint8 connect_pullup);
+
+
+#endif // DIO_H_INCLUDED
